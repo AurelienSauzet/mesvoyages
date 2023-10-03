6185777c6943be4595893553b4ebd6c8763cd36a
AurelienSauzet <aurelien.sauzet@orange.fr>
2023-10-03 10:48:43 +0200

    Phase 12: ORM (modification d'un enregistrement, création d'un formulaire).

--- src/Controller/admin/AdminVoyagesController.php
+++ src/Controller/admin/AdminVoyagesController.php
@@ -2,6 +2,7 @@
 namespace App\Controller\admin;
 
 use App\Entity\Visite;
+use App\Form\VisiteType;
 use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -25,7 +26,8 @@ class AdminVoyagesController extends AbstractController {
      * 
      * @param VisiteRepository $repository
      */
-    public function __construct(VisiteRepository $repository) {
+    public function __construct(VisiteRepository $repository)
+    {
         $this->repository = $repository;
     }
 
@@ -33,21 +35,45 @@ class AdminVoyagesController extends AbstractController {
      * @Route("/admin", name="admin.voyages")
      * @return Response
      */
-    public function index(): Response{
-        $visites = $this->repository->findAllOrderBy('datecreation', 'DESC');
+    public function index(): Response
+    {
+        $visites = $this->repository->findAllOrderBy("datecreation", "DESC");
         return $this->render("admin/admin.voyages.html.twig", [
             'visites' => $visites
         ]);
     }
-
+    
     /**
      * @Route("/admin/suppr/{id}", name="admin.voyage.suppr")
      * @param Visite $visite
      * @return Response
      */
-    public function suppr(Visite $visite): Response{
+    public function suppr(Visite $visite): Response
+    {
         $this->repository->remove($visite, true);
         return $this->redirectToRoute('admin.voyages');
     }
-
+    
+    /**
+     * @Route("/admin/edit/{id}", name="admin.voyage.edit")
+     * @param Visite $visite
+     * @param Request $request
+     * @return Response
+     */
+    public function edit(Visite $visite, Request $request): Response
+    {
+        $formVisite = $this->createForm(VisiteType::class, $visite);
+        
+        $formVisite->handleRequest($request);
+        if($formVisite->isSubmitted() && $formVisite->isValid())
+        {
+            $this->repository->add($visite, true);
+            return $this->redirectToRoute("admin.voyages");
+        }
+        
+        return $this->render("admin/admin.voyage.edit.html.twig", [
+            "visite" => $visite,
+            "formvisite" => $formVisite->createView()
+        ]);
+    }
 }
